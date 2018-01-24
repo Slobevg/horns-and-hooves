9cf9817e045c0c1e38539292f8c057394543821b
Slobevg <slobodskoy.e.g@gmail.com>
2018-01-24 18:15:16 +0300

    initial spring boot project

---
+++ src/main/java/com/slobevg/hornsandhooves/HornsAndHoovesApplication.java
@@ -0,0 +1,12 @@
+package com.slobevg.hornsandhooves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HornsAndHoovesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HornsAndHoovesApplication.class, args);
+	}
+}
